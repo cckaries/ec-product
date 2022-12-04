ac12d9972592d4b5b3e6a2cf51bdb5b58aba9800
Shou-Chi Chang <cckaries@gmail.com>
2022-12-05 01:34:27 +0800

    feat(reviews): add 'Review'

---
+++ src/pages/Product/Reviews/Review/Review.js
@@ -0,0 +1,42 @@
+import { format, getTime } from 'date-fns';
+import cx from 'classnames';
+
+import Ratings from '../../../../shared/Ratings/Ratings';
+import Image from '../../../../shared/Image/Image';
+
+import styles from './Review.module.scss';
+
+const Review = ({
+  username = '',
+  ratingScore = 0.0,
+  description = '',
+  dateTs = 0,
+  images = [],
+  customClass,
+}) => {
+  return (
+    <div className={cx(styles.Container, customClass)}>
+      <div className={styles.Title}>
+        <div className={styles.Left}>
+          <div className={styles.Name}>{username}</div>
+          <div className={styles.Rating}>
+            <Ratings score={ratingScore} />
+          </div>
+        </div>
+        <div className={styles.Date}>
+          {!!dateTs && format(getTime(dateTs * 1000), 'MMM d, yyyy')}
+        </div>
+      </div>
+      <p>{description}</p>
+      {!!images.length && (
+        <div className={styles.Images}>
+          {images.map((img, idx) => (
+            <Image key={idx} src={img} width="100px" height="80px" />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Review;
